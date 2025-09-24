c960a6350d7d0d9e18afea2bc1ed2af700ae1a33
usernayeem <93907431+usernayeem@users.noreply.github.com.>
2025-09-24 20:38:04 +0600

    add Navbar component and integrate Public Sans font

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="w-11/12 h-[10rem] m-auto flex items-center justify-between">
+      <div>
+        <img src="../images/logo.png" alt="logo" />
+      </div>
+      <div>
+        <button className="bg-[#3BA334] text-white py-[1rem] px-[2.6rem] public-sans-700 rounded-[0.8rem] fill-[#39A432] drop-shadow-xl/25">
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+};
